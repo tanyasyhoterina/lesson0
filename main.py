78004c3109f27b7617b052f270713e5cd4d8abc7
tanyasyhoterina <syhoterina@gmail.com>
2025-01-18 17:45:07 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+# 1st program
+print((9**0.5)*5)
+#2nd program
+print(9.99>9.98 and 1000!=1000.1)
+#3rd program
+print(2*2+2)
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+#4th program
+print("123.456")
+print(type(float("123.456")))
+print(123.456*10)
+print(int(1234.56))
+print(1234%10)
+print(1234%100)
+print(1234%1000)
 
-
-print('Hello, word')
